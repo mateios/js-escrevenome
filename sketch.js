512aaf9a65719582d190256c02a814ed70ba337b
matheux <145468083+mateios@users.noreply.github.com>
2023-10-19 08:59:01 -0300

    adicionado js e css

---
+++ sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+    createCanvas(10000, 10000);
+  }
+  function setup() {
+  
+  createCanvas(1000, 1000);
+  
+  background("white");
+  
+  }
+  
+  function draw() {
+  
+  stroke("blue");
+  
+  fill("red");
+  
+  if(mouseIsPressed){
+  
+  rect(mouseX, mouseY, 20, 35);
+  
+  }
+  
+  }
